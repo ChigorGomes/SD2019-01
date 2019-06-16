2c05f5a8ecbfb7e5ebb453861ac696f0e2b02c85
Cicero Higor <chgs@icomp.ufam.edu.br>
2019-06-16 09:31:51 -0400

    Colocado o menu e gerado o ícone da aplicação

--- LeafGarden/APP/app/src/main/java/com/app/leafgarden/TelaMenu.java
+++ LeafGarden/APP/app/src/main/java/com/app/leafgarden/TelaMenu.java
@@ -3,6 +3,8 @@ package com.app.leafgarden;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -59,5 +61,21 @@ public class TelaMenu extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.end:
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
