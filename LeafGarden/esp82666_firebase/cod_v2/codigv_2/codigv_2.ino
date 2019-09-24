108c1da67932dd5eaa44b791c0523c7f4d07262e
Cicero Higor <chgs@icomp.ufam.edu.br>
2019-09-24 17:47:56 -0400

    Adicionando codigo V2 do NodeMcu

--- LeafGarden/esp82666_firebase/cod_v2/codigv_2/codigv_2.ino
+++ LeafGarden/esp82666_firebase/cod_v2/codigv_2/codigv_2.ino
@@ -14,6 +14,24 @@
 #include <DHT.h>
 #include <DHT_U.h>
 #include <BH1750FVI.h>
+#include <TimeLib.h> 
+#include <WiFiUdp.h>
+
+
+// NTP Servers:
+IPAddress timeServer(132, 163, 4, 101); // time-a.timefreq.bldrdoc.gov
+// IPAddress timeServer(132, 163, 4, 102); // time-b.timefreq.bldrdoc.gov
+// IPAddress timeServer(132, 163, 4, 103); // time-c.timefreq.bldrdoc.gov
+
+//const int timeZone = 1;     // Central European Time
+//const int timeZone = -5;  // Eastern Standard Time (USA)
+const int timeZone = -4;  // Eastern Daylight Time (USA)
+//const int timeZone = -8;  // Pacific Standard Time (USA)
+//const int timeZone = -7;  // Pacific Daylight Time (USA)
+
+
+WiFiUDP Udp;
+unsigned int localPort = 8888;  // local port to listen for UDP packets
 
 
 
@@ -161,6 +179,11 @@ void setup() {
   LightSensor.begin();
   sensorTemperatureSoil.begin();
   sensorTemperatureSoil.getAddress(sensorDS18B20Address, 0);
+  Udp.begin(localPort); //verificar
+  setSyncProvider(getNtpTime);
+  MAC = String(WiFi.macAddress());
+
+
   delay(5000);
 }
 
@@ -181,22 +204,21 @@ void salvaHistorico(){
   moisureInformation = getMoisureInformation(A0);
   luminosidade = getEnvironmentLightIntensityInformation(LightSensor);
   umidadeAmbiente = getEnvironmentHumidityInformation(dht);
-
   stringTS = int(temperaturaSolo);
   stringTA = int(temperaturaAmbiente);
   stringMI = int(moisureInformation);
   stringLI = int(luminosidade);
   stringUA = int(umidadeAmbiente);
-  MAC = String(WiFi.macAddress());
   String randnumber= String(random(500000));
-
-
+  String data = enviarData();
   Firebase.setInt("Historico/"+MAC+"/"+randnumber+"/temperaturasolo",stringTS);
   Firebase.setInt("Historico/"+MAC+"/"+randnumber+"/temperaturaambiente",stringTA);
   Firebase.setInt("Historico/"+MAC+"/"+randnumber+"/umidadesolo",stringMI);
   Firebase.setInt("Historico/"+MAC+"/"+randnumber+"/luminosidade",stringLI);
   Firebase.setInt("Historico/"+MAC+"/"+randnumber+"/umidadeambiente",stringUA);
   Firebase.setString("Historico/"+MAC+"/"+randnumber+"/idsensor",MAC);
+  Firebase.setString("Historico/"+MAC+"/"+randnumber+"/horario",data);
+
 
 }
 
@@ -207,14 +229,11 @@ void firebaseTempoReal(){
   moisureInformation = getMoisureInformation(A0);
   luminosidade = getEnvironmentLightIntensityInformation(LightSensor);
   umidadeAmbiente = getEnvironmentHumidityInformation(dht);
-
   stringTS = int(temperaturaSolo);
   stringTA = int(temperaturaAmbiente);
   stringMI = int(moisureInformation);
   stringLI = int(luminosidade);
   stringUA = int(umidadeAmbiente);
-  MAC = String(WiFi.macAddress());
-
   Firebase.setInt("Sensor/"+MAC+"/temperaturasolo",stringTS);
   Firebase.setInt("Sensor/"+MAC+"/temperaturaambiente",stringTA);
   Firebase.setInt("Sensor/"+MAC+"/umidadesolo",stringMI);
@@ -232,11 +251,90 @@ unsigned long tempoMili; //http://www.bosontreinamentos.com.br/eletronica/arduin
 /*--------------------------------------------------------------------- LOOP ---------------------------------------------------------*/
 void loop() {
   tempoMili = millis();
-  if((tempoMili % 30000)==0 ){ // ler a cada 30 segundos
-    firebaseTempoReal();
-    Serial.println("lendo");
-  }if((tempoMili % 7200000)==0 ){ // salva a cada 2hrs 
-    salvaHistorico();
-    Serial.println("salvo");
+      if((tempoMili % 30000)==0 ){ // ler a cada 30 segundos
+        firebaseTempoReal();
+        Serial.println("lendo");
+      }if((tempoMili % 60000)==0 ){ // salva a cada 2hrs  7200000
+        salvaHistorico();
+        Serial.println("salvo");
+      }
+
+}
+
+
+
+
+// função do tempo
+time_t prevDisplay = 0; // when the digital clock was displayed
+
+String enviarData(){
+  String hora;
+  String data;
+   if (timeStatus() != timeNotSet) {
+    if (now() != prevDisplay) { //update the display only if time has changed
+      prevDisplay = now();
+        hora = String(hour());
+        String minuto = String(minute());
+        hora = hora+":"+minuto;
+        String dia = String(day());
+        String mes = String(month());
+        data = String(year());
+        data= hora+"-"+dia+"/"+mes+"/"+data;
+    }  
+    return data;
+}
+
+}
+
+
+/*-------- NTP code ----------*/
+
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+
+time_t getNtpTime()
+{
+  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+  Serial.println("Transmit NTP Request");
+  sendNTPpacket(timeServer);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = Udp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      Serial.println("Receive NTP Response");
+      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      unsigned long secsSince1900;
+      // convert four bytes starting at location 40 to a long integer
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
   }
+  Serial.println("No NTP Response :-(");
+  return 0; // return 0 if unable to get the time
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress &address)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:                 
+  Udp.beginPacket("pool.ntp.br", 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
 }
